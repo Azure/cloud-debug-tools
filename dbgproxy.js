47cc4276018b0692248a4fb9a0e85bc23e941d18
Reiley Yang <reyang@microsoft.com>
2017-11-16 12:41:00 -0800

    simplify the CLI usage

--- dbgproxy.js
+++ dbgproxy.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
-var net = require('net');
-var path = require('path');
-var WebSocketClient = require('websocket').client;
-var VERSION = require('./package.json').version;
+const { execSync } = require('child_process');
+const net = require('net');
+const path = require('path');
+const WebSocketClient = require('websocket').client;
+const HTTP_PLATFORM_DEBUG_PORT = 8898;
+const VERSION = require('./package.json').version;
+
+function shell(cmdline) {
+  var retval = execSync(cmdline, {stdio: [process.stdin, 'pipe', process.stderr]});
+  return JSON.parse(retval);
+}
 
 var argv = process.argv.slice(1);
 if (argv.length < 2 || argv.length > 3) {
@@ -11,14 +18,10 @@ if (argv.length < 2 || argv.length > 3) {
   process.exit(-1);
 }
 
-var idx = argv[1].lastIndexOf('@');
-if (idx === -1) {
-  help();
-  process.exit(-1);
-}
-
-var functionId = argv[1].slice(idx + 1);
-var auth = argv[1].slice(0, idx);
+// TODO: need to consider global Azure vs. regional data centers and Edge
+var functionId = argv[1];
+var idx = functionId.indexOf('.');
+var functionName = idx === -1 ? functionId : functionId.slice(0, idx);
 var options = {host: '127.0.0.1', port: 8898, backlog: 1};
 var wsoptions = {'Cache-Control': 'no-cache', 'Pragma': 'no-cache'};
 
@@ -35,30 +38,69 @@ if (argv.length === 3) {
   }
 }
 
+console.log(`Function App:                 ${JSON.stringify(functionId)}`);
+
+process.stdout.write(`Subscription:                 `);
+var currentSubscription = shell('az account show');
+console.log(JSON.stringify(currentSubscription.name), `(ID = ${JSON.stringify(currentSubscription.id)})`);
+process.stdout.write(`Resource Group:               `);
+var arrFunctionApps = shell('az functionapp list --query "[].{name: name, resourceGroup: resourceGroup}"');
+var objFunctionApps = {};
+for (var i = 0; i < arrFunctionApps.length; i++) {
+  objFunctionApps[arrFunctionApps[i].name.toLowerCase()] = arrFunctionApps[i];
+}
+
+if (!objFunctionApps.hasOwnProperty(functionName.toLowerCase())) {
+  console.error(`ERROR: cannot find ${JSON.stringify(functionName)} under current subscription ${JSON.stringify(currentSubscription.name)}`)
+  process.exit(-1);
+}
+
+var resourceGroup = objFunctionApps[functionName.toLowerCase()].resourceGroup;
+console.log(JSON.stringify(resourceGroup));
+
+process.stdout.write(`Fetch debug settings:         `);
+var objCredential = shell(`az functionapp deployment list-publishing-profiles --resource-group ${resourceGroup} --name ${functionName} --query "[?contains(publishMethod, 'MSDeploy')].{name: userName, password: userPWD}"`)[0];
+
+var bRemoteDebugEnabled = Boolean(shell(`az webapp config appsettings list --resource-group ${resourceGroup} --name ${functionName} --query "[?contains(name, 'HTTP_PLATFORM_DEBUG_PORT')]"`).length);
+console.log(`done`);
+
+if (!bRemoteDebugEnabled) {
+  process.stdout.write(`Switch to 64bit:              `);
+  shell(`az resource update --id /subscriptions/${currentSubscription.id}/resourceGroups/${resourceGroup}/providers/Microsoft.Web/sites/${functionName}/config/web --set properties.use32BitWorkerProcess=false`);
+  console.log(`done`);
+
+  process.stdout.write(`Enable WebSocket:             `);
+  shell(`az resource update --id /subscriptions/${currentSubscription.id}/resourceGroups/${resourceGroup}/providers/Microsoft.Web/sites/${functionName}/config/web --set properties.webSocketsEnabled=true`);
+  console.log(`done`);
+
+  process.stdout.write(`Set JAVA_OPTS:                `);
+  shell(`az webapp config appsettings set --resource-group ${resourceGroup} --name ${functionName} --settings JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=127.0.0.1:${HTTP_PLATFORM_DEBUG_PORT}"`);
+  console.log(`done`);
+
+  process.stdout.write(`Set HTTP_PLATFORM_DEBUG_PORT: `);
+  shell(`az webapp config appsettings set --resource-group ${resourceGroup} --name ${functionName} --settings HTTP_PLATFORM_DEBUG_PORT=${HTTP_PLATFORM_DEBUG_PORT}`);
+  console.log(`done`);
+}
+
+console.log(`Remote debugging is now enabled on ${JSON.stringify(functionId)}`);
+
+
 function help() {
   console.log([
-    'Usage: dbgproxy <user name>:<password>@<FQDN> [<binding host>:<port>]', // TODO: [<affinity cookie>]
+    'Usage: dbgproxy <FQDN> [<binding host>:<port>]', // TODO: [<affinity cookie>]
     '',
     'Sample:',
     '',
-    '    dbgproxy admin:password@my-function.azurewebsites.net',
-    '    dbgproxy admin:password@my-function.azurewebsites.net *:8000',
-    '    dbgproxy admin:password@my-function.azurewebsites.net 0.0.0.0:8898',
-    '    dbgproxy admin:password@my-function.azurewebsites.net 127.0.0.1:8898',
-    '    dbgproxy admin:password@my-function.azurewebsites.net localhost:8898',
-    '    dbgproxy admin:password@my-function.azurewebsites.net ::1:8898',
-    '    dbgproxy admin:password@my-function.azurewebsites.net 0:0:0:0:0:0:0:1:8898',
+    '    dbgproxy my-function.azurewebsites.net',
+    '    dbgproxy my-function.azurewebsites.net *:8000',
+    '    dbgproxy my-function.azurewebsites.net 0.0.0.0:8898',
+    '    dbgproxy my-function.azurewebsites.net 127.0.0.1:8898',
+    '    dbgproxy my-function.azurewebsites.net localhost:8898',
+    '    dbgproxy my-function.azurewebsites.net ::1:8898',
+    '    dbgproxy my-function.azurewebsites.net 0:0:0:0:0:0:0:1:8898',
     '',
   ].join('\r\n'));
   console.log('dbgproxy@' + VERSION, path.resolve(__dirname, 'dbgproxy'));
-}
-
-function getWebSocketUriFromFunctionId(functionId) {
-  var idx = functionId.indexOf('.');
-  if (idx === -1) {
-    return 'wss://' + functionId + '.scm.azurewebsites.net' + '/DebugSiteExtension/JavaDebugSiteExtension.ashx';
-  }
-  return 'wss://' + functionId.slice(0, idx) + '.scm' + functionId.slice(idx) + '/DebugSiteExtension/JavaDebugSiteExtension.ashx';
 }
 
 var server = net.createServer();
@@ -117,7 +159,7 @@ server.on('connection', function(socket) {
       socket.destroy();
     });
 
-    wsclient.connect(getWebSocketUriFromFunctionId(functionId), null, null, wsoptions, {auth: auth,});
+    wsclient.connect(`wss://${functionName}.scm.azurewebsites.net/DebugSiteExtension/JavaDebugSiteExtension.ashx`, null, null, wsoptions, {auth: objCredential.name + ':' + objCredential.password});
   }
 
   socket.on('data', function(data) {
